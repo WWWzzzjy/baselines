f18a8b3b8c5183773881e1fa84cd1c72d68fe4cc
akai <2780896955@qq.com>
2026-04-30 05:39:26 +0800

    兼容

--- baseline_swerank/src/eval_beir_sbert_canonical.py
+++ baseline_swerank/src/eval_beir_sbert_canonical.py
@@ -36,6 +36,15 @@ if not hasattr(DynamicCache, 'get_usable_length'):
     def _get_usable_length(self, new_seq_length, layer_idx=0):
         return self.get_seq_length(layer_idx)
     DynamicCache.get_usable_length = _get_usable_length
+
+# DynamicCache.to_legacy_cache was removed in newer transformers; patch it back
+if not hasattr(DynamicCache, 'to_legacy_cache'):
+    def _to_legacy_cache(self):
+        return tuple(
+            (self.key_cache[layer_idx], self.value_cache[layer_idx])
+            for layer_idx in range(len(self.key_cache))
+        )
+    DynamicCache.to_legacy_cache = _to_legacy_cache
 import csv
 from sentence_transformers import SentenceTransformer
 from collections import defaultdict
